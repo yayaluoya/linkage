e46a93ce52453fc012b5757fd59f741ab0d73808
yayaluoya <yayaluoya@sina.com>
2021-12-20 10:46:50 +0800

    增加一个rem的修改工具

--- web/src/element/WindowState.ts
+++ web/src/element/WindowState.ts
@@ -2,7 +2,7 @@ import { InstanceTool } from "com_utils/InstanceTool";
 import { createThrottleFun } from "@/utils/throttleAntiShake";
 import { BaseElementState } from "./BaseElementState";
 
-type eventType = 'null';
+type eventType = 'resize';
 
 /**
  * Window的状态
@@ -15,10 +15,14 @@ export class WindowState extends BaseElementState<eventType> {
     //
     constructor() {
         super(document.documentElement);
-        //给documentElement绑定的滚动事件会不生效，这里重新绑定下
+        //给documentElement绑定的滚动事件会不生效，这里重新绑定下，同时清空默认绑定的滚动事件
         this.off('scroll');
         window.addEventListener('scroll', createThrottleFun((e: any) => {
             this.emit('scroll', e);
         }, 50));
+        //添加滚动事件
+        window.addEventListener('resize', () => {
+            this.emit('resize');
+        });
     }
 }
