5e2a588edac2cd1c521148d0438c32302807a00e
yayaluoya <yayaluoya@sina.com>
2024-02-03 14:36:05 +0800

    上传包的逻辑

--- .node/uploadDIst.js
+++ .node/uploadDIst.js
@@ -15,135 +15,13 @@ try {
 }
 
 /**
- * 上传打包后的代码
- */
-async function uploadDist() {
-  const keyUrl = path.join(__dirname, '../.ssh/a');
-  if (
-    !fs
-      .statSync(keyUrl, {
-        throwIfNoEntry: false,
-      })
-      ?.isFile()
-  ) {
-    console.log(`
-请配置服务器ssh连接私钥以便上传包文件到服务器
-可以在当前项目执行目录下执行ssh-keygen -f ./.ssh/a命令生成秘钥并把公钥（.pub结尾的文件）交给后端添加到服务器中
-然后重新上传就可以了
-      `);
-    return;
-  }
-  startSync({
-    /** 主机地址 */
-    host: '',
-    /** 端口号 */
-    port: 22,
-    /** 用户名 */
-    username: 'root',
-    /** 私钥字符串 */
-    privateKey: fs.readFileSync(keyUrl),
-    /** 合并本地的配置 */
-    ...localConfig,
-    /** 同步列表 */
-    syncList: [
-      {
-        key: 'config',
-        title: '配置文件',
-        paths: [
-          {
-            local: path.join(__dirname, '../.local/.keep'),
-            remote: `${serverRootUrl}/.local/.keep`,
-          },
-        ],
-      },
-      /** 因为后端不会打包依赖，所以需要把依赖的全局模块传上去 */
-      {
-        key: 'global',
-        title: '全局模块',
-        paths: [
-          {
-            local: path.join(__dirname, '../dist'),
-            remote: `${serverRootUrl}/dist`,
-            ignored: [
-              path.join(__dirname, '../dist/**/*.d.ts').replace(/\\+/g, '/'),
-              path.join(__dirname, '../dist/**/*.d.ts.map').replace(/\\+/g, '/'),
-            ],
-          },
-          {
-            local: path.join(__dirname, '../package.json'),
-            remote: `${serverRootUrl}/package.json`,
-          },
-          {
-            local: path.join(__dirname, '../ecosystem.config.js'),
-            remote: `${serverRootUrl}/ecosystem.config.js`,
-          },
-        ],
-        laterF(connF) {
-          return connF().then((conn) => {
-            return upatePm2(conn).finally(() => {
-              conn.end();
-            });
-          });
-        },
-      },
-      {
-        key: 'server',
-        title: '后端包',
-        paths: [
-          {
-            local: path.join(__dirname, '../server/_data'),
-            remote: `${serverRootUrl}/server/_data`,
-          },
-          {
-            local: path.join(__dirname, '../server/_localData/.keep'),
-            remote: `${serverRootUrl}/server/_localData/.keep`,
-          },
-          {
-            local: path.join(__dirname, '../server/dist'),
-            remote: `${serverRootUrl}/server/dist`,
-            ignored: [
-              path.join(__dirname, './server/dist/**/*.js.map').replace(/\\+/g, '/'),
-              path.join(__dirname, './server/dist/**/*.d.ts').replace(/\\+/g, '/'),
-              path.join(__dirname, './server/dist/**/*.d.ts.map').replace(/\\+/g, '/'),
-            ],
-          },
-          {
-            local: path.join(__dirname, '../server/package.json'),
-            remote: `${serverRootUrl}/server/package.json`,
-          },
-        ],
-        laterF(connF) {
-          return connF().then((conn) => {
-            return upatePm2(conn).finally(() => {
-              conn.end();
-            });
-          });
-        },
-      },
-      {
-        key: 'web',
-        title: '前端包',
-        paths: [
-          {
-            local: path.join(__dirname, '../web/dist/index.html'),
-            remote: `${serverRootUrl}/web/dist/index.html`,
-          },
-        ],
-      },
-    ],
-    /** ssh2的连接配置 */
-    connectConfig: {},
-    /** 是否监听 */
-    watch: false,
-  });
-}
-
-/**
  * 更新pm2
  * @param {import('ssh2').Client} conn
  */
 function upatePm2(conn) {
   return new Promise((r) => {
+    console.log(`更新 ${serverRootUrl}/ecosystem.config.js`);
+    //
     conn.exec(`pm2 start ${serverRootUrl}/ecosystem.config.js`, (err, stream) => {
       if (err) {
         console.log('出错了', err);
@@ -162,6 +40,139 @@ function upatePm2(conn) {
         });
     });
   });
+}
+
+/**
+ * 上传打包后的代码
+ */
+async function uploadDist() {
+  const keyUrl = path.join(__dirname, '../.ssh/a');
+  if (
+    !fs
+      .statSync(keyUrl, {
+        throwIfNoEntry: false,
+      })
+      ?.isFile()
+  ) {
+    console.log(`
+请配置服务器ssh连接私钥以便上传包文件到服务器
+可以在当前项目执行目录下执行ssh-keygen -f ./.ssh/a命令生成秘钥并把公钥（.pub结尾的文件）交给后端添加到服务器中
+然后重新上传就可以了
+      `);
+    return;
+  }
+  /** 是否要更新pm2 */
+  let ifUpdatePm2 = false;
+  //
+  startSync(
+    {
+      /** 主机地址 */
+      host: '',
+      /** 端口号 */
+      port: 22,
+      /** 用户名 */
+      username: 'root',
+      /** 私钥字符串 */
+      privateKey: fs.readFileSync(keyUrl),
+      /** 合并本地的配置 */
+      ...localConfig,
+      /** 同步列表 */
+      syncList: [
+        {
+          key: 'config',
+          title: '配置文件',
+          paths: [
+            {
+              local: path.join(__dirname, '../.local/.keep'),
+              remote: `${serverRootUrl}/.local/.keep`,
+            },
+          ],
+        },
+        /** 因为后端不会打包依赖，所以需要把依赖的全局模块传上去 */
+        {
+          key: 'global',
+          title: '全局模块',
+          paths: [
+            {
+              local: path.join(__dirname, '../dist'),
+              remote: `${serverRootUrl}/dist`,
+              ignored: [
+                path.join(__dirname, '../dist/**/*.d.ts').replace(/\\+/g, '/'),
+                path.join(__dirname, '../dist/**/*.d.ts.map').replace(/\\+/g, '/'),
+              ],
+            },
+            {
+              local: path.join(__dirname, '../package.json'),
+              remote: `${serverRootUrl}/package.json`,
+            },
+            {
+              local: path.join(__dirname, '../ecosystem.config.js'),
+              remote: `${serverRootUrl}/ecosystem.config.js`,
+            },
+          ],
+          laterF() {
+            ifUpdatePm2 = true;
+          },
+        },
+        {
+          key: 'server',
+          title: '后端包',
+          paths: [
+            {
+              local: path.join(__dirname, '../server/_data'),
+              remote: `${serverRootUrl}/server/_data`,
+            },
+            {
+              local: path.join(__dirname, '../server/_localData/.keep'),
+              remote: `${serverRootUrl}/server/_localData/.keep`,
+            },
+            {
+              local: path.join(__dirname, '../server/dist'),
+              remote: `${serverRootUrl}/server/dist`,
+              ignored: [
+                path.join(__dirname, './server/dist/**/*.js.map').replace(/\\+/g, '/'),
+                path.join(__dirname, './server/dist/**/*.d.ts').replace(/\\+/g, '/'),
+                path.join(__dirname, './server/dist/**/*.d.ts.map').replace(/\\+/g, '/'),
+              ],
+            },
+            {
+              local: path.join(__dirname, '../server/package.json'),
+              remote: `${serverRootUrl}/server/package.json`,
+            },
+          ],
+          laterF() {
+            ifUpdatePm2 = true;
+          },
+        },
+        {
+          key: 'web',
+          title: '前端包',
+          paths: [
+            {
+              local: path.join(__dirname, '../web/dist/index.html'),
+              remote: `${serverRootUrl}/web/dist/index.html`,
+            },
+          ],
+        },
+      ],
+      laterF(connF) {
+        if (!ifUpdatePm2) {
+          return;
+        }
+        return connF().then((conn) => {
+          return Promise.resolve(upatePm2(conn)).finally(() => {
+            conn.end();
+          });
+        });
+      },
+      /** ssh2的连接配置 */
+      connectConfig: {},
+      /** 是否监听 */
+      watch: false,
+    },
+    undefined,
+    true,
+  );
 }
 
 uploadDist();
